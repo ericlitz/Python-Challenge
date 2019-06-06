ec01104838b76bf97db21f6cc0c602031673c64e
Eric Litz <46330553+ericlitz@users.noreply.github.com>
2019-06-06 18:35:22 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,63 +1,90 @@
-print("This Years Finacial Data")
+print("This Years election data")
 import os, csv
 from pathlib import Path
 
 
-input_file = Path("C:\\", "Users", "erict", "Documents", "Bootcamp", "Homework","Python-Homework", "PyBank", "budget_data.csv")
-
-total_months = []
-total_profit = []
-monthly_profit_change = []
+csv_file_path = Path("C:\\", "Users", "erict", "Documents", "Bootcamp", "Homework","Python-Homework", "PyPoll", "election_data.csv")
 
 
-with open(input_file,newline="", encoding="utf-8") as budget:
+total_votes = 0
+khan_votes = 0
+correy_votes = 0
+li_votes = 0
+otooley_votes = 0
 
 
-   csvreader = csv.reader(budget,delimiter=",")
+with open(csv_file_path,newline="", encoding="utf-8") as elections:
 
+
+   csvreader = csv.reader(elections,delimiter=",")
 
    header = next(csvreader)
 
+
    for row in csvreader:
 
-
-       total_months.append(row[0])
-       total_profit.append(int(row[1]))
+       total_votes +=1
 
 
-   for i in range(len(total_profit)-1):
+       if row[2] == "Khan":
+           khan_votes +=1
+       elif row[2] == "Correy":
+           correy_votes +=1
+       elif row[2] == "Li":
+           li_votes +=1
+       elif row[2] == "O'Tooley":
+           otooley_votes +=1
 
-       monthly_profit_change.append(total_profit[i+1]-total_profit[i])
 
-max_increase_value = max(monthly_profit_change)
-max_decrease_value = min(monthly_profit_change)
-
-max_increase_month = monthly_profit_change.index(max(monthly_profit_change)) + 1
-max_decrease_month = monthly_profit_change.index(min(monthly_profit_change)) + 1
+candidates = ["Khan", "Correy", "Li","O'Tooley"]
+votes = [khan_votes, correy_votes,li_votes,otooley_votes]
 
 
-print("Financial Analysis")
-print("----------------------------")
-print(f"Total Months: {len(total_months)}")
-print(f"Total: ${sum(total_profit)}")
-print(f"Average Change: {round(sum(monthly_profit_change)/len(monthly_profit_change),2)}")
-print(f"Greatest Increase in Profits: {total_months[max_increase_month]} (${(str(max_increase_value))})")
-print(f"Greatest Decrease in Profits: {total_months[max_decrease_month]} (${(str(max_decrease_value))})")
+dict_candidates_and_votes = dict(zip(candidates,votes))
+key = max(dict_candidates_and_votes, key=dict_candidates_and_votes.get)
 
-output_file = Path("C:\\", "Users", "erict", "Documents", "Bootcamp", "Homework","Python-Homework", "PyBank", "budget_data.txt")
+
+khan_percent = (khan_votes/total_votes) *100
+correy_percent = (correy_votes/total_votes) * 100
+li_percent = (li_votes/total_votes)* 100
+otooley_percent = (otooley_votes/total_votes) * 100
+
+print(f"Election Results")
+print(f"----------------------------")
+print(f"Total Votes: {total_votes}")
+print(f"----------------------------")
+print(f"Khan: {khan_percent:.3f}% ({khan_votes})")
+print(f"Correy: {correy_percent:.3f}% ({correy_votes})")
+print(f"Li: {li_percent:.3f}% ({li_votes})")
+print(f"O'Tooley: {otooley_percent:.3f}% ({otooley_votes})")
+print(f"----------------------------")
+print(f"Winner: {key}")
+print(f"----------------------------")
+
+
+output_file = Path("C:\\", "Users", "erict", "Documents", "Bootcamp", "Homework","Python-Homework", "PyPoll", "Election_Results_Summary.txt")
 
 with open(output_file,"w") as file:
 
-   file.write("Financial Analysis")
+
+   file.write(f"Election Results")
    file.write("\n")
-   file.write("----------------------------")
+   file.write(f"----------------------------")
    file.write("\n")
-   file.write(f"Total Months: {len(total_months)}")
+   file.write(f"Total Votes: {total_votes}")
    file.write("\n")
-   file.write(f"Total: ${sum(total_profit)}")
+   file.write(f"----------------------------")
    file.write("\n")
-   file.write(f"Average Change: {round(sum(monthly_profit_change)/len(monthly_profit_change),2)}")
+   file.write(f"Khan: {khan_percent:.3f}% ({khan_votes})")
    file.write("\n")
-   file.write(f"Greatest Increase in Profits: {total_months[max_increase_month]} (${(str(max_increase_value))})")
+   file.write(f"Correy: {correy_percent:.3f}% ({correy_votes})")
    file.write("\n")
-   file.write(f"Greatest Decrease in Profits: {total_months[max_decrease_month]} (${(str(max_decrease_value))})")
+   file.write(f"Li: {li_percent:.3f}% ({li_votes})")
+   file.write("\n")
+   file.write(f"O'Tooley: {otooley_percent:.3f}% ({otooley_votes})")
+   file.write("\n")
+   file.write(f"----------------------------")
+   file.write("\n")
+   file.write(f"Winner: {key}")
+   file.write("\n")
+   file.write(f"----------------------------")
