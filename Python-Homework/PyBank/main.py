1d8910c8433e4b8def162008b1985719ab90a691
Eric Litz <46330553+ericlitz@users.noreply.github.com>
2019-06-06 10:02:57 -0500

    Add files via upload

---
+++ Python-Homework/PyBank/main.py
@@ -0,0 +1,9 @@
+import os
+
+import csv
+
+csv_path = os.path.join("budget_data.csv")
+
+print("Financial Analysis")
+
+print("------------------")
---
+++ Python-Homework/PyPoll/main.py
@@ -0,0 +1,10 @@
+import os
+
+import csv
+
+csv_path = os.path.join("election_data.csv")
+
+print("Election Results")
+
+print("------------------")
+
